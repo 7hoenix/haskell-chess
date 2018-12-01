2d843c70d24b4490ea00388ad0fd143f160da88f
7hoenix <jpholzmann@gmail.com>
2018-12-01 14:49:51 -0600

    Print empty board

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,14 +3,24 @@ module Lib
     , emptyBoard
     ) where
 
-data Board = Board Position
+import Data.List
+import Data.Maybe
+
+data Board = Board [Square]
 
 instance Show Board where
-  show (Board pos) = show pos
+  show (Board squares) = show squares
 
 data Piece = Monarch | Hand | Rook | Bishop | Knight | Pawn deriving (Show, Eq)
 
 data Position = Position ValidIndex ValidIndex deriving (Show, Eq)
+
+data Square = Square (Maybe Piece) Position deriving (Eq)
+
+instance Show Square where
+  show (Square Nothing _) = " "
+  show (Square (Just Monarch) _) = "M"
+  show (Square (Just _) _) = "a"
 
 type ValidIndex = Int
 
@@ -21,7 +31,9 @@ validIndex n | n < 0 || n > 7 = Left OutOfBoundsError
   | otherwise                 = Right n
 
 emptyBoard :: Board
-emptyBoard = Board $ Position 0 0
+emptyBoard = Board $ concatMap (\row ->
+  map (\column -> Square Nothing $ Position row column) [0..7]
+                         ) [0..7]
 
 printBoard :: Board -> IO ()
-printBoard board = putStrLn $ show board
+printBoard board = print board
