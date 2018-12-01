5718492f8eb6e4fd1e6b889f1fcd70864105810e
7hoenix <jpholzmann@gmail.com>
2018-12-01 11:28:03 -0600

    Map out some types

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,27 @@
 module Lib
-    ( someFunc
+    ( printBoard
+    , emptyBoard
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data Board = Board Position
+
+instance Show Board where
+  show (Board pos) = show pos
+
+data Piece = Monarch | Hand | Rook | Bishop | Knight | Pawn deriving (Show, Eq)
+
+data Position = Position ValidIndex ValidIndex deriving (Show, Eq)
+
+type ValidIndex = Int
+
+data OutOfBoundsError = OutOfBoundsError deriving (Show)
+
+validIndex :: Int -> Either OutOfBoundsError ValidIndex
+validIndex n | n < 0 || n > 7 = Left OutOfBoundsError
+  | otherwise                 = Right n
+
+emptyBoard :: Board
+emptyBoard = Board $ Position 0 0
+
+printBoard :: Board -> IO ()
+printBoard board = putStrLn $ show board
