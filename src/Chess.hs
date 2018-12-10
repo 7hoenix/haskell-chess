6b4457529014f8a1a8c46c1204dc670c560ee0c4
7hoenix <jpholzmann@gmail.com>
2018-12-09 19:47:26 -0500

    Add legal function for monarch

--- src/Chess.hs
+++ src/Chess.hs
@@ -1,14 +1,22 @@
 module Chess
     ( printBoard
     , emptyBoard
-    , playBoard
-    , place
-    , whiteKnight
-    , remove
-    , move
     , get
-    , a1, a2
-    , h8
+    , move
+    , place
+    , playBoard
+    , remove
+    , legal
+    , Team(..)
+    , monarch, hand, rook, bishop, knight, pawn
+    , a1, a2, a3, a4, a5, a6, a7, a8
+    , b1, b2, b3, b4, b5, b6, b7, b8
+    , c1, c2, c3, c4, c5, c6, c7, c8
+    , d1, d2, d3, d4, d5, d6, d7, d8
+    , e1, e2, e3, e4, e5, e6, e7, e8
+    , f1, f2, f3, f4, f5, f6, f7, f8
+    , g1, g2, g3, g4, g5, g6, g7, g8
+    , h1, h2, h3, h4, h5, h6, h7, h8
     ) where
 
 import Data.List
@@ -51,14 +59,9 @@ whiteHand :: Piece
 whiteHand =
   Piece White Hand
 
-a1 :: Position
-a1 = Position 0 0
-
-a2 :: Position
-a2 = Position 1 0
-
-h8 :: Position
-h8 = Position 7 7
+legal :: Position -> Board -> [Position]
+legal position (Board board) =
+  []
 
 place :: Piece -> Position -> Board -> Board
 place piece position (Board board) =
@@ -97,6 +100,233 @@ playBoard :: Board
 playBoard = foldr (\(pos, piece) currentBoard -> place piece pos currentBoard)
                 emptyBoard
                 [(a1, whiteKnight), (a2, whiteBishop), (h8, whiteHand)]
+
+-- Piece constructor functions
+
+monarch :: Team -> Piece
+monarch team =
+  Piece team Monarch
+
+hand :: Team -> Piece
+hand team =
+  Piece team Hand
+
+rook :: Team -> Piece
+rook team =
+  Piece team Rook
+
+bishop :: Team -> Piece
+bishop team =
+  Piece team Bishop
+
+knight :: Team -> Piece
+knight team =
+  Piece team Knight
+
+pawn :: Team -> Piece
+pawn team =
+  Piece team Pawn
+
+-- All Possible positions
+
+a1 :: Position
+a1 = Position 0 0
+
+a2 :: Position
+a2 = Position 1 0
+
+a3 :: Position
+a3 = Position 2 0
+
+a4 :: Position
+a4 = Position 3 0
+
+a5 :: Position
+a5 = Position 4 0
+
+a6 :: Position
+a6 = Position 5 0
+
+a7 :: Position
+a7 = Position 6 0
+
+a8 :: Position
+a8 = Position 7 0
+
+
+b1 :: Position
+b1 = Position 0 1
+
+b2 :: Position
+b2 = Position 1 1
+
+b3 :: Position
+b3 = Position 2 1
+
+b4 :: Position
+b4 = Position 3 1
+
+b5 :: Position
+b5 = Position 4 1
+
+b6 :: Position
+b6 = Position 5 1
+
+b7 :: Position
+b7 = Position 6 1
+
+b8 :: Position
+b8 = Position 7 1
+
+
+c1 :: Position
+c1 = Position 0 2
+
+c2 :: Position
+c2 = Position 1 2
+
+c3 :: Position
+c3 = Position 2 2
+
+c4 :: Position
+c4 = Position 3 2
+
+c5 :: Position
+c5 = Position 4 2
+
+c6 :: Position
+c6 = Position 5 2
+
+c7 :: Position
+c7 = Position 6 2
+
+c8 :: Position
+c8 = Position 7 2
+
+
+d1 :: Position
+d1 = Position 0 3
+
+d2 :: Position
+d2 = Position 1 3
+
+d3 :: Position
+d3 = Position 2 3
+
+d4 :: Position
+d4 = Position 3 3
+
+d5 :: Position
+d5 = Position 4 3
+
+d6 :: Position
+d6 = Position 5 3
+
+d7 :: Position
+d7 = Position 6 3
+
+d8 :: Position
+d8 = Position 7 3
+
+
+e1 :: Position
+e1 = Position 0 4
+
+e2 :: Position
+e2 = Position 1 4
+
+e3 :: Position
+e3 = Position 2 4
+
+e4 :: Position
+e4 = Position 3 4
+
+e5 :: Position
+e5 = Position 4 4
+
+e6 :: Position
+e6 = Position 5 4
+
+e7 :: Position
+e7 = Position 6 4
+
+e8 :: Position
+e8 = Position 7 4
+
+
+f1 :: Position
+f1 = Position 0 5
+
+f2 :: Position
+f2 = Position 1 5
+
+f3 :: Position
+f3 = Position 2 5
+
+f4 :: Position
+f4 = Position 3 5
+
+f5 :: Position
+f5 = Position 4 5
+
+f6 :: Position
+f6 = Position 5 5
+
+f7 :: Position
+f7 = Position 6 5
+
+f8 :: Position
+f8 = Position 7 5
+
+
+g1 :: Position
+g1 = Position 0 6
+
+g2 :: Position
+g2 = Position 1 6
+
+g3 :: Position
+g3 = Position 2 6
+
+g4 :: Position
+g4 = Position 3 6
+
+g5 :: Position
+g5 = Position 4 6
+
+g6 :: Position
+g6 = Position 5 6
+
+g7 :: Position
+g7 = Position 6 6
+
+g8 :: Position
+g8 = Position 7 6
+
+
+h1 :: Position
+h1 = Position 0 7
+
+h2 :: Position
+h2 = Position 1 7
+
+h3 :: Position
+h3 = Position 2 7
+
+h4 :: Position
+h4 = Position 3 7
+
+h5 :: Position
+h5 = Position 4 7
+
+h6 :: Position
+h6 = Position 5 7
+
+h7 :: Position
+h7 = Position 6 7
+
+h8 :: Position
+h8 = Position 7 7
 
 -- PRIVATE --
 
--- test/ChessSpec.hs
+++ test/ChessSpec.hs
@@ -3,27 +3,46 @@ module ChessSpec where
 import Test.Hspec
 import Data.Either
 
-import Chess (place, emptyBoard, whiteKnight, remove, a1, h8, get, move)
+import Chess (legal, place, emptyBoard, remove, get, move
+             , knight, monarch
+             , Team(..)
+             , a1, a2, a3, a4, a5, a6, a7, a8
+             , b1, b2, b3, b4, b5, b6, b7, b8
+             , c1, c2, c3, c4, c5, c6, c7, c8
+             , d1, d2, d3, d4, d5, d6, d7, d8
+             , e1, e2, e3, e4, e5, e6, e7, e8
+             , f1, f2, f3, f4, f5, f6, f7, f8
+             , g1, g2, g3, g4, g5, g6, g7, g8
+             , h1, h2, h3, h4, h5, h6, h7, h8
+             )
 
 spec :: Spec
 spec = do
   describe "Chess.place" $ do
     it "returns a new board with the piece placed on it" $ do
-      Chess.place whiteKnight a1 emptyBoard `shouldNotBe` emptyBoard
+      Chess.place (knight White) a1 emptyBoard `shouldNotBe` emptyBoard
 
   describe "Chess.remove" $ do
     it "returns a new board with the square cleared" $ do
-      (Chess.remove a1 . Chess.place whiteKnight a1) emptyBoard `shouldBe` emptyBoard
+      (Chess.remove a1 . Chess.place (knight White) a1) emptyBoard `shouldBe` emptyBoard
 
   describe "Chess.get" $ do
     it "returns the piece from a square if occupied" $ do
-      (Chess.get a1 . Chess.place whiteKnight a1) emptyBoard `shouldBe` (Just whiteKnight)
+                                           (Chess.get a1 . Chess.place (knight White) a1) emptyBoard `shouldBe` (Just (knight White))
     it "returns nothing if empty" $ do
       Chess.get a1 emptyBoard `shouldBe` Nothing
 
   describe "Chess.move" $ do
      it "no longer has piece in previous square" $ do
-       (Chess.get a1 . Chess.move a1 h8 . Chess.place whiteKnight a1) emptyBoard `shouldBe` Nothing
+       (Chess.get a1 . Chess.move a1 h8 . Chess.place (knight White) a1) emptyBoard `shouldBe` Nothing
      it "has piece in new square" $ do
-       (Chess.get h8 . Chess.move a1 h8 . Chess.place whiteKnight a1) emptyBoard `shouldBe` (Just whiteKnight)
+       (Chess.get h8 . Chess.move a1 h8 . Chess.place (knight White) a1) emptyBoard `shouldBe` (Just (knight White))
 
+  describe "Chess.legal" $ do
+    it "knows monarch movement - corner" $ do
+      (Chess.legal a1 . Chess.place (monarch White) a1) emptyBoard `shouldBe` [a2, b1, b2]
+    -- it "knows monarch movement - center" $ do
+    --   (Chess.legal . Chess.place whiteMonarch b2) emptyBoard `shouldBe` [a1, a2, a3, b1, b3, c1, c2, c3]
+
+
+-- Add support for checks
